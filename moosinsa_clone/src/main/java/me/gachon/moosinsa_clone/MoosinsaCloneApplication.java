62aa0435aeaee3245e240c3b7e3a9ba833115a3a
mygithub05253 <kik328288@gmail.com>
2025-07-30 17:58:19 +0900

    무신사 클론 프로젝트 첫 번째 푸시

---
+++ moosinsa_clone/src/main/java/me/gachon/moosinsa_clone/MoosinsaCloneApplication.java
@@ -0,0 +1,13 @@
+package me.gachon.moosinsa_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoosinsaCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoosinsaCloneApplication.class, args);
+    }
+
+}
---
+++ moosinsa_clone/src/test/java/me/gachon/moosinsa_clone/MoosinsaCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gachon.moosinsa_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoosinsaCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
