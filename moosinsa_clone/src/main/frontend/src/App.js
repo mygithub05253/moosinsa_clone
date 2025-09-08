f4b4520cec00e27525fee823eb63504599b54e53
mygithub05253 <kik328288@gmail.com>
2025-09-08 16:08:11 +0900

    병합 완료 최종 코드

--- moosinsa_clone/src/main/frontend/src/App.js
+++ moosinsa_clone/src/main/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 // './global/style' 파일에서 GlobalStyle을 named export 방식으로 내보냈으므로,
 // import 할 때 중괄호 {}를 사용하여 정확하게 'GlobalStyle'이라는 이름으로 가져옵니다.
 import { GlobalStyle } from "./global/GlobalStyles";
@@ -8,6 +8,10 @@ import MainPage from './pages/Main/MainPage';
 import SignupPage from './pages/Main/SignupPage';
 import UserListPage from './pages/Main/UserListPage';
 import LoginPage from './pages/Main/LoginPage';
+import Items from './Items';
+import ItemDetail from './components/ItemDetail';
+import ItemCreate from './components/ItemCreate';
+import ItemSizeCreate from "./components/ItemSizeCreate";
 function App() {
   return (
     <BrowserRouter>
@@ -24,6 +28,14 @@ function App() {
           <Route path="/users" element={<UserListPage />} />
           {/* '/login' 경로에 LoginPage 컴포넌트를 연결하는 Route를 추가합니다. */}
           <Route path="/login" element={<LoginPage />} />
+          {/* 리스트 */}
+          <Route path="/items" element={<Items />} />
+          {/* 상세 */}
+          <Route path="/items/:itemId" element={<ItemDetail />} />
+          {/* 루트로 오면 /items로 */}
+          <Route path="/" element={<Navigate to="/items" replace />} />
+          <Route path="/items/create" element={<ItemCreate />} />
+          <Route path="/items/create-sizes/:itemId" element={<ItemSizeCreate />} />
         </Route>
       </Routes>
     </BrowserRouter>
